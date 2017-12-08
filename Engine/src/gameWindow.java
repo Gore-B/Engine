ad899b712e74936381c656bb002be6e97f2ff1aa
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-08 14:30:26 -0800

    added JFrame

--- Engine/src/gameWindow.java
+++ Engine/src/gameWindow.java
@@ -1,10 +1,22 @@
+import java.awt.Graphics2D;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 public class gameWindow extends Thread{
 	
 	public boolean running;
+	public JFrame mainWindow;
+	public static JPanel drawBoard;
+	public static BufferedImage image;
+	public static Graphics2D g;
 	
 	public gameWindow() {
-		
+		createAndShowGUI();
 		this.run();
 	}
 	
@@ -21,4 +33,26 @@ public class gameWindow extends Thread{
 		
 	}
 
+	
+	
+	/**
+	 * Creates the main game window.
+	 */
+	private void createAndShowGUI() {
+		mainWindow = new JFrame();
+		mainWindow.setSize(1280, 720);
+		mainWindow.setVisible(true);
+		
+		drawBoard = new JPanel();
+		drawBoard.setSize(1280, 720);
+		mainWindow.add(drawBoard);
+		drawBoard.setVisible(true);
+		
+		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainWindow.setFocusable(true);
+		mainWindow.setResizable(false);
+		mainWindow.requestFocus();
+		mainWindow.setVisible(true);
+	}
+	
 }
