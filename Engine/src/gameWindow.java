487d0fac741166a74760d5d370a7bcecb45ee918
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-08 14:21:35 -0800

    Frame pacing

--- Engine/src/gameWindow.java
+++ Engine/src/gameWindow.java
@@ -1,6 +1,24 @@
 
 public class gameWindow extends Thread{
+	
+	public boolean running;
+	
 	public gameWindow() {
-		System.out.println("test");
+		
+		this.run();
 	}
+	
+	public void run(){
+		long nextFrame =  (System.nanoTime() + 16666667);
+		
+		while(true) {
+			if(System.nanoTime() >= nextFrame) {
+				nextFrame += 16666667;
+				//frame
+			}
+			
+		}
+		
+	}
+
 }
